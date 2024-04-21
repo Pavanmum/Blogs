c55ee5cd0dbaa27055aefce16c3250951dc8550c
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2024-04-21 22:51:28 +0530

    Refactor app.js and index.js to use express.json() middleware and update dependencies

--- index.js
+++ index.js
@@ -10,45 +10,75 @@ connectDatabase();
 
 const app = express();
 
+app.use(express.json());
 
 app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
+
+app.use(express.json("urlencoded", { extended: true }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(logger("dev"));
+
+app.use(express.json());
 app.use(cookieParser());
 
 
-
-
+// Import routes
 const authRoutes = require("./routes/authRoutes");
 const articleRoutes = require("./routes/articleRoutes");
 const commentRoutes = require("./routes/commentRoutes");
+const { default: mongoose } = require("mongoose");
 
+
+// Use Routes
+
+ 
 
 app.use('/api/v1', authRoutes);
 app.use('/api/v1/article', articleRoutes);
 app.use('/api/v1/comment', commentRoutes);
 
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Blog API");
 
+
+app.get('/', (req, res) => {
+  const message = {
+    status: 'success',
+    code: 200,
+    message: 'Welcome To Blog api',
+  };
+
+  res.status(200).json(message);
 });
 
-app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong";
-  res.status(errorStatus).send(errorMessage);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
 });
 
+app.use((err , req , res ,next ) =>{
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went Wrong"
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  return res.status(errorStatus).send(errorMessage)
+
+})
 
 
 process.on("unhandledRejection", (err) => {
-  console.error(`Unhandled Promise Rejection: ${err}`);
-  process.exit(1);
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`Shutting down the server for unhandle promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
+
+
+module.exports = app;
